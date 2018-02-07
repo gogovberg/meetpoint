6fbd03fbb405f7056df1cab07ca383e04a612e7e
gogovberg <gogov91a@gmail.com>
2018-02-07 16:47:03 +0100

    new commit

--- MeetpointPrinter/Pages/SettingsPage.xaml.cs
+++ MeetpointPrinter/Pages/SettingsPage.xaml.cs
@@ -14,15 +14,24 @@ using System.Windows.Shapes;
 
 namespace MeetpointPrinter.Pages
 {
+
     /// <summary>
     /// Interaction logic for SettingsPage.xaml
     /// </summary>
     public partial class SettingsPage : Window
     {
+        private DateTime downTime;
+        private object downSender;
+
         public SettingsPage()
         {
             InitializeComponent();
             (Application.Current as App).CurrentPage = "Settings page";
+            cbTemplateSize.Items.Add("150x150");
+            cbTemplateSize.Items.Add("170x170");
+            cbTemplateSize.Items.Add("150x170");
+            cbTemplateSize.Items.Add("170x200");
+            cbTemplateSize.Items.Add("150x200");
         }
 
         private void btnCancel_Click(object sender, RoutedEventArgs e)
@@ -33,6 +42,39 @@ namespace MeetpointPrinter.Pages
         private void btnSave_Click(object sender, RoutedEventArgs e)
         {
             //save logic
+        }
+
+        private void cbTemplateSize_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            string text = (sender as ComboBox).SelectedItem as string;
+            string[] size = text.Split('x');
+            imgPriview.Height = int.Parse(size[0]);
+            imgPriview.Width = int.Parse(size[1]);
+
+        }
+       
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.downSender = sender;
+                this.downTime = DateTime.Now;
+            }
+        }
+
+        private void Border_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Released &&
+                sender == this.downSender)
+            {
+                TimeSpan timeSinceDown = DateTime.Now - this.downTime;
+                if (timeSinceDown.TotalMilliseconds < 500)
+                {
+                    // Do click
+                }
+            }
         }
     }
 }
