9a43d029fef267972830d5ce0b04ae36fa49ef4b
gogovberg <gogov91a@gmial.com>
2018-04-18 19:40:44 +0200

    data options

--- MeetpointPrinterNew/MeetpointPrinterNew/Pages/SetupPagePrintTemplate.xaml.cs
+++ MeetpointPrinterNew/MeetpointPrinterNew/Pages/SetupPagePrintTemplate.xaml.cs
@@ -20,17 +20,19 @@ namespace MeetpointPrinterNew.Pages
     /// </summary>
     public partial class SetupPagePrintTemplate : Page
     {
-        private int pageType = -1;
-        public int SetupPageType { get { return this.pageType; } }
+        private int _pageType = -1;
+        private List<CheckBox> _dataOptions;
+
+        public int SetupPageType { get { return this._pageType; } }
+        
         public SetupPagePrintTemplate()
         {
-            pageType = 2;
+            _pageType = 2;
             InitializeComponent();
-
             lblPrintingDevice.Content = "SET LABEL TEMPLATE";
+
+            _dataOptions = new List<CheckBox>();
         }
-
-
 
         private void cbTwoHunderd_Checked(object sender, RoutedEventArgs e)
         {
@@ -58,7 +60,6 @@ namespace MeetpointPrinterNew.Pages
             cbLayoutQLT.IsChecked = false;
             cbLayoutQRB.IsChecked = false;
             cbLayoutQRT.IsChecked = false;
-           
         }
 
         private void cbLayoutQRT_Checked(object sender, RoutedEventArgs e)
@@ -68,7 +69,6 @@ namespace MeetpointPrinterNew.Pages
             cbLayoutQLB.IsChecked = false;
             cbLayoutQLT.IsChecked = false;
             cbLayoutQRB.IsChecked = false;
-            
         }
 
         private void cbLayoutQRB_Checked(object sender, RoutedEventArgs e)
@@ -87,6 +87,7 @@ namespace MeetpointPrinterNew.Pages
             cbLayoutQLT.IsChecked = false;
             cbLayoutQRB.IsChecked = false;
             cbLayoutQRT.IsChecked = false;
+
         }
 
         private void cbLayoutQLT_Checked(object sender, RoutedEventArgs e)
@@ -96,6 +97,7 @@ namespace MeetpointPrinterNew.Pages
             cbLayoutQLB.IsChecked = false;
             cbLayoutQRB.IsChecked = false;
             cbLayoutQRT.IsChecked = false;
+
         }
 
         private void cbLayoutQLB_Checked(object sender, RoutedEventArgs e)
@@ -105,6 +107,53 @@ namespace MeetpointPrinterNew.Pages
             cbLayoutQLT.IsChecked = false;
             cbLayoutQRB.IsChecked = false;
             cbLayoutQRT.IsChecked = false;
+
+        }
+
+
+        private void cbName_Checked(object sender, RoutedEventArgs e)
+        {
+            DataOptionsLogic(sender);
+        }
+        private void cbSurName_Checked(object sender, RoutedEventArgs e)
+        {
+            DataOptionsLogic(sender);
+        }
+
+        private void cbCompanyName_Checked(object sender, RoutedEventArgs e)
+        {
+            DataOptionsLogic(sender);
+        }
+
+        private void cbFunctionName_Checked(object sender, RoutedEventArgs e)
+        {
+            DataOptionsLogic(sender);
+        }
+
+        private void cbCountryName_Checked(object sender, RoutedEventArgs e)
+        {
+            DataOptionsLogic(sender);
+        }
+
+        private void cbGroupName_Checked(object sender, RoutedEventArgs e)
+        {
+            DataOptionsLogic(sender);
+
+        }
+
+        private void DataOptionsLogic(Object sender)
+        {
+            CheckBox cb = (CheckBox)sender;
+            if (_dataOptions.Count < 3)
+            {
+                _dataOptions.Insert(0, cb);
+            }
+            else
+            {
+                _dataOptions[2].IsChecked = false;
+                _dataOptions.RemoveAt(2);
+                _dataOptions.Insert(0, cb);
+            }
         }
     }
 }
